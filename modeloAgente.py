c45116d04a119b9ecca2b82bb87ad3add3863956
Jose Villalobos <villalobosmartinezjosedavid@gmail.com>
2019-08-08 14:10:55 -0600

    AG:readTxt

--- modeloAgente.py
+++ modeloAgente.py
@@ -1,3 +1,26 @@
+"""
+Instituto Tecnológico de Costa Rica
+Escuela de compiutación
+Curso: Inteligencia Artificial
+Profesor: Esteban Arias Méndez
+Estudiantes:
+Camila Víquez Alpízar 2014152891
+Fabricio Alvarado Esquivel 2016089643
+"""
+
+def readTxtFile():
+    matrix = []
+    with open("text.txt") as fp:
+        line = fp.readline()
+        while line:
+            word = line.split()
+            matrix.append(word)
+            line = fp.readline()
+    print(matrix)
+
+
+readTxtFile()
+
 def printMatrix(row, column):
     result = []
     for i in range(row):
@@ -6,5 +29,4 @@ def printMatrix(row, column):
     for i in result:
         print (i)
 
-printMatrix(3,4)
     
